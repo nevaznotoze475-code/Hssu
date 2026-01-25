c2f5d79101212f840536843b9ebce82c4ea6c6b2
nevaznotoze475-code <nevaznotoze475@gmail.com>
2026-01-25 21:39:54 +0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,36 @@
+TOKEN = "7154351713:AAHhaBNM5AUmYosR3zdFeaZBypSgOL_97AA" #сюда токен бота
+
+admins_id = [
+    5858391454
+    8063185389
+]
+
+SUBGRAM_TOKEN = "3eb29f0601c89c477b953f1e579bb5fe8cc27ed05f167c661b8a3114ca7506fe" # токен subgram
+HIVIEWS_TOKEN = "UA6TXBT1RD0ZRAGRHRY5R"
+
+
+admin_username = "@Skwlqa" #свой username
+admin_link = "https://t.meSkwlqa"
+
+GIFT_AMOUNT = 0.3 # ежедневый гифт
+DAILY_COOLDOWN = 24 * 60 * 60 # не трогать
+DELAY_TIME = 160 # не трогать
+
+#основной канал
+channel_osn = "https://t.me/teamraz"
+#чат
+chater = "https://t.me/piarvbm"
+id_chat = -1002768211871
+#канал выплат
+channel_viplat = "https://t.me/mebld"
+channel_viplat_id = -1003498880772
+#канал выигрышей мини-игры:
+channel_link = ""
+id_channel_game = -10024016
+
+#Обязательная подписка
+required_subscription = []
+
+#кнопка в старте по запросу
+beta_url = ""
+beta_name = ""
